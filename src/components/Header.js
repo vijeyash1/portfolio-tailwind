e42eae1b0dcd7834c678b72bb2d3ab3a10535968
vijeyash <vijeyash@gmail.com>
2022-02-09 17:32:49 +0530

    added nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Header() {
-  return <div>header</div>;
+  const menuItems = [
+    {
+      title: "HOME",
+      KEY: "/",
+    },
+    {
+      title: "CONTACTS",
+      KEY: "/contacts",
+    },
+    {
+      title: "PROJECTS",
+      KEY: "/projects",
+    },
+  ];
+  return (
+    <div className="text-white font-mont">
+      <div className="flex bg-theme justify-between items-center p-2 shadow-lg">
+        <div>
+          <h1 className="text-4xl font-bold">VIJESH</h1>
+        </div>
+        <div className="flex">
+          {menuItems.map((item) => {
+            return (
+              <li className="list-none mx-5" key={item.KEY}>
+                <Link to={`/${item.KEY}`}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
