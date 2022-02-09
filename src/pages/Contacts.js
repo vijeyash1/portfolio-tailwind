f47e67bc1fc0949558856c11c22d31ee5a39dea4
vijeyash <vijeyash@gmail.com>
2022-02-09 16:47:39 +0530

    added folder structure

---
+++ src/pages/Contacts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function Contacts() {
+  return (
+    <Layout>
+      <div>Contacts</div>
+    </Layout>
+  );
+}
+
+export default Contacts;
