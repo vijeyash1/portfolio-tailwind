ddd6e0199da66362cc15d50298ec7ffb37b9abc3
vijeyash <vijeyash@gmail.com>
2022-02-10 15:58:44 +0530

    added home page animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,32 @@
 import React from "react";
 import Layout from "../components/Layout";
-
+// for animation lotte animations
 function Home() {
   return (
     <Layout>
-      <div>Home</div>
+      <div>
+        <div className="bg-theme h-screen">
+          <div className="grid md:grid-cols-1 grid-cols-2 h-screen items-center ">
+            <div className="h-1/2">
+              <lottie-player
+                src="https://assets2.lottiefiles.com/private_files/lf30_WdTEui.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+            <div className="font-bold text-white md:px-5">
+              <h1 className="text-7xl md:4xl">
+                Hi I am <b className="text-blue-200">VIJESH</b>
+              </h1>
+              <h1 className="text-4xl md:text-2xl">
+                Am a <b className="text-red-500">React Developer</b>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
