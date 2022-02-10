afcdfa342882984f52d633295c14ef38837d6a79
vijeyash <vijeyash@gmail.com>
2022-02-10 17:03:03 +0530

    technologies i use

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout";
+import {
+  FaBootstrap,
+  FaCss3,
+  FaHtml5,
+  FaReact,
+  FaAngular,
+} from "react-icons/fa";
+import { SiTailwindcss, SiJavascript, SiGoland } from "react-icons/si";
+import { DiNodejs } from "react-icons/di";
+
 // for animation lotte animations
 function Home() {
   return (
@@ -27,6 +37,52 @@ function Home() {
                 Am a <b className="text-red-500">React Developer</b>
               </h1>
             </div>
+          </div>
+        </div>
+        <div className="mt-24">
+          <h1 className="text-4xl text-blue-800 font-bold text-center my-8">
+            Technologies I Use
+          </h1>
+          <div className="grid md:grid-cols-1 grid-cols-4 z-10">
+            <FaReact
+              size={180}
+              className="text-cyan-400 w-full text-center mt-20 animate-bounce"
+            />
+            <FaBootstrap
+              size={180}
+              color="#EC1801"
+              className="w-full text-center mt-20 animate-pulse"
+            />
+            <FaCss3
+              size={180}
+              color="#1B6AC6"
+              className=" w-full text-center mt-20 animate-pulse"
+            />
+            <FaHtml5
+              size={180}
+              color="#FB7A24"
+              className=" w-full text-center mt-20 animate-bounce"
+            />
+            <SiGoland
+              size={180}
+              color="#CC0000"
+              className=" w-full text-center mt-20 animate-pulse"
+            />
+            <SiTailwindcss
+              size={180}
+              color="#FB7A24"
+              className=" w-full text-center mt-20 animate-bounce"
+            />
+            <SiJavascript
+              size={180}
+              color="#FDC72E"
+              className=" w-full text-center mt-20 animate-bounce"
+            />
+            <DiNodejs
+              size={180}
+              color="#3C873A"
+              className=" w-full text-center mt-20 animate-pulse"
+            />
           </div>
         </div>
       </div>
